e0be5b57e0a973a1baefba793f6d1bc35e5f5c34
Chandu <chandu4it@gmail.com>
2020-12-03 22:22:09 +0530

    Added data to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name:'Fresho Onion - Medium/Kanda- Madhyam',description:'Onion can fill your kitchen with a thick spicy aroma. It is a common base vegetable in most Indian dishes, thanks to the wonderful flavor that it adds to any dish. 
+',price:'27',image_url:'https://www.bigbasket.com/media/uploads/p/l/20001190_10-fresho-onion.jpg');
+
+Product.create(name:'Fresho Brinjal - Purple (Bharta)/Wangi-Jambhali ( Bharit)',description:'Brinjal Purple Bharta has glossy skin and comes in wide range of shapes and sizes. The flesh is creamy and spongy with numerous tiny, smooth edible seeds. 
+',price:'57',image_url:'https://www.bigbasket.com/media/uploads/p/l/30000435_11-fresho-brinjal-purple-bharta.jpg');
+
+Product.create(name:'Fresho Carrot English/Gajar English (Lal)',description:'A popular sweet-tasting root vegetable, Carrots are narrow and cone shaped. They have thick, fleshy, deeply colored root, which grows underground, and feathery green leaves that emerge above the ground. While these greens are fresh tasting and slightly bitter, the carrot roots are crunchy textured with a sweet and minty aromatic taste. Fresho brings you the flavour and richness of the finest bright orange, crispy and juicy carrots. 
+',price:'39',image_url:'https://www.bigbasket.com/media/uploads/p/l/10000271_13-fresho-carrot-ooty.jpg');
+
+Product.create(name:'Fresho Chilli - Hybrid',description:'Chilli Hybrid contains viscous, sharp and pungent taste; however the sharpness can differ depending on the variety of chilly. It is a staple of Indian diets. They get their hot flavor from the compound capsaicin, which happens naturally in chillies. It is wealthy in Vitamin A & C and includes Vitamin E, some B vitamins, potassium & iron. 
+',price:'15',image_url:'https://www.bigbasket.com/media/uploads/p/l/30000645_11-fresho-chilli-hybrid.jpg');
+
+Product.create(name:'Fresho Cucumber Color - Grade A/Kakdi Rangit- Grade A',description:'Mangalore cucumber is oval shaped, with yellow stripes on green background and a white, firm flesh with edible seeds. It is a sambar cucumber as it is widely used to prepare sambar in Mangalore. It can be consumed raw and be used for wide number of recipes like sambar, curries, chutney, dosa, idli, Mangalore cucumber raitha, etc. 
+',price:'27',image_url:'https://www.bigbasket.com/media/uploads/p/l/10000100_9-fresho-cucumber-mangalore.jpg');
+
+Product.create(name:'Fresho Drumstick - Grade A/Shewgyachya shenga',description:'Drumsticks have long, dark green bumpy pods running along the length and fleshy pulp inside the pod encasing each seed. These pods have a faint bitter taste combined with slight sweetness. 
+',price:'27',image_url:'https://www.bigbasket.com/media/uploads/p/l/10000110_20-fresho-drumstickmoringa.jpg');
+
+Product.create(name:'Fresho Onion - Medium/Kanda- Madhyam Local',description:'Onion can fill your kitchen with a thick spicy aroma. It is a common base vegetable in most Indian dishes, thanks to the wonderful flavor that it adds to any dish. 
+',price:'27',image_url:'https://www.bigbasket.com/media/uploads/p/l/20001190_10-fresho-onion.jpg');
+
+Product.create(name:'Fresho Ridge Gourd/Dodka',description:'Ridge Gourds are long vegetables with a ridged dark green skin and white spongy flesh that contains numerous seeds inside. They are rich in fiber and very prominent in Indian cooking. 
+',price:'27',image_url:'https://www.bigbasket.com/media/uploads/p/l/10000176_15-fresho-ridge-gourd.jpg');
